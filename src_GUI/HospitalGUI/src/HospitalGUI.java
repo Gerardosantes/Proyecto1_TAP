0d09c2954d503ec9433ca38e3728e8df1e48ce19
JFUENTES91 <larajael338@gmail.com>
2026-05-18 00:30:39 -0600

    codigo actualizado

--- src_GUI/HospitalGUI/src/HospitalGUI.java
+++ src_GUI/HospitalGUI/src/HospitalGUI.java
@@ -49,6 +49,34 @@ public class HospitalGUI extends JFrame {
         //  3.1 - MENU PRINCIPAL
 
 
+        // BOTON REGISTRAR
+        btnRegistro = new JButton("Registrar Turno");
+        btnRegistro.setBounds(150,70,180,35);
+        estiloBoton(btnRegistro);
+        btnRegistro.setVisible(false);
+        add(btnRegistro);
+
+        // BOTON ACTIVOS
+        btnActivos = new JButton("Turnos Activos");
+        btnActivos.setBounds(150,120,180,35);
+        estiloBoton(btnActivos);
+        btnActivos.setVisible(false);
+        add(btnActivos);
+
+        // BOTON FINALIZAR
+        btnFinalizar = new JButton("Finalizar Turno");
+        btnFinalizar.setBounds(150,170,180,35);
+        estiloBoton(btnFinalizar);
+        btnFinalizar.setVisible(false);
+        add(btnFinalizar);
+
+        // BOTON SALIR
+        btnSalir = new JButton("Salir");
+        btnSalir.setBounds(150,220,180,35);
+        estiloBoton(btnSalir);
+        btnSalir.setVisible(false);
+        add(btnSalir);
+
 
         //  2.2 - EVENTO LOGIN
 
@@ -56,7 +84,52 @@ public class HospitalGUI extends JFrame {
 
 
         //  3.2 - FUNCIONES DEL MENU
+       
+        // REGISTRO
+        btnRegistro.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
+                JOptionPane.showMessageDialog(null,
+                        "Se esta trabajando en ello.",
+                        "Avance del Sistema",
+                        JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+
+        // TURNOS ACTIVOS
+        btnActivos.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                JOptionPane.showMessageDialog(null,
+                        "Se esta trabajando en ello.",
+                        "Avance del Sistema",
+                        JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+
+        // FINALIZAR TURNO
+        btnFinalizar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                JOptionPane.showMessageDialog(null,
+                        "Se esta trabajando en ello.",
+                        "Avance del Sistema",
+                        JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+
+        // SALIR DEL SISTEMA
+        btnSalir.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                System.exit(0);
+            }
+        });
+    }
 
   
     //  1.2 - ESTILOS VISUALES
@@ -72,7 +145,21 @@ public class HospitalGUI extends JFrame {
     
     //  3.3 - MOSTRAR MENU
 
+    public void mostrarMenu() {
 
+        // OCULTAR LOGIN
+        lblUsuario.setVisible(false);
+        txtUsuario.setVisible(false);
+        lblContra.setVisible(false);
+        txtContra.setVisible(false);
+        btnIngresar.setVisible(false);
+
+        // MOSTRAR MENU
+        btnRegistro.setVisible(true);
+        btnActivos.setVisible(true);
+        btnFinalizar.setVisible(true);
+        btnSalir.setVisible(true);
+    }
 
 
     // MAIN PRINCIPAL
